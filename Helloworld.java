55475f2f819118906491670b67bc8a2f170b1923
Mohib Siddiqui <mohibsiddiqui91@gmail.com>
2021-08-07 04:13:53 +0000

    added the math logic & README.md, fifth commit

--- Helloworld.java
+++ Helloworld.java
@@ -1,11 +1,7 @@
-//This is a Helloworld java program, second addition
-
+//This is a sample program
 public class HelloWorld {
    public static void main(String[] args)  {
       // Prints "Hello, World" in the terminal window.
       System.out.println("Hello, World");
-      // Added a comment line for new print, forth addition	
-      System.out.println("Hello Git!!");
-      System.out.println("Hello AWS");
    }
 }	
---
+++ math.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+   public static void main(String[] args)  {
+      // Prints "Welcome message" in the terminal window.
+      System.out.println("Welcome to Maths");
+   }
+}	
+
