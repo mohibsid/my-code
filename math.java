43a3ed7453b596a087e9383f97a53e63fd52ab9c
Mohib Siddiqui <mohibsiddiqui91@gmail.com>
2021-08-08 20:22:56 +0000

    Fixed a bug n: 1111

--- math.java
+++ math.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
    public static void main(String[] args)  {
       // Prints "Welcome message" in the terminal window.
-      System.out.println("Welcome to Maths");
+      System.out.println("Welcome to Mathematics");
    }
 }	
 
